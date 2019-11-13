d4668861aa655c7bd44a2b1732bca8dbbefbfb05
DmitrySar <youshin.d@gmail.com>
2019-11-13 11:51:24 +0300

    edit method getHTML()

--- src/HTTPReader.java
+++ src/HTTPReader.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+
 class HTTPReader {
 
     private String url;
+
+    private String codePage = "UTF-8";
 
     public HTTPReader(String url) {
         this.url = url;
     }
 
-    public String getHTML() {
-        return url;
+    public String getHTML() throws IOException {
+        return new Scanner(new URL(url).openStream(), codePage).useDelimiter("\\A").next();
     }
 
 
