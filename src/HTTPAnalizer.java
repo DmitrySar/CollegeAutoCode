e8a10b32a927b53763648d483ee36fed1fb366b3
DmitrySar <youshin.d@gmail.com>
2019-11-14 17:26:21 +0300

    modify analizer

--- src/HTTPAnalizer.java
+++ src/HTTPAnalizer.java
@@ -1,15 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 class HTTPAnalizer {
 
     private HTTPReader reader;
+
+    private final int TIME_ZONE = 4;
 
     public HTTPAnalizer(HTTPReader reader) {
         this.reader = reader;
     }
 
     public String getResult() throws IOException {
-        return reader.getHTML();
+    	String result = "";
+    	String[] doc = reader.getHTML().split("\n");
+    	for (String s: doc) {
+    		if (s.matches(".*utcTime:.*")) {
+    			result = s;
+    		}
+    	}
+    	result = result.replaceAll(".*: ", "");
+    	result = result.replaceAll(",.*", "");
+    	GregorianCalendar calendar = new GregorianCalendar();
+    	//TimeZone tz = TimeZone.getTimeZone("Russia/Saransk");
+    	calendar.setGregorianChange(new Date(new Long(result)));
+    	calendar.setTimeZone(TimeZone.getTimeZone("Russia/Saransk"));
+    	result = calendar.HOUR + ":" + calendar.MINUTE + ":" + calendar.SECOND;
+        return result;
     }
 
 }
--- src/HTTPController.java
+++ src/HTTPController.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 class HTTPController {
 
@@ -8,7 +9,8 @@ class HTTPController {
 
     private HTTPResult result;
 
-    private String url = "https://ya.ru";
+    private String url = "https://www.timeserver.ru/cities/ru/saransk";
+    //private String url = "file:///d:/projects/CollegeAutoCode/src/HTTPAnalizer.java";
 
     public static void main(String[] args) throws IOException {
     	HTTPController controller = new HTTPController();
@@ -16,6 +18,7 @@ class HTTPController {
     }
 
     private void start() throws IOException {
+        //GregorianCalendar calendar = new GregorianCalendar();
         reader = new HTTPReader(url);
         analizer = new HTTPAnalizer(reader);
         result = new HTTPResult(analizer);
