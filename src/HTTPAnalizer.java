40b4555a47b43c2812595961a65e87ef5ebae579
DmitrySar <youshin.d@gmail.com>
2019-11-15 11:29:46 +0300

    edit logic

--- src/HTTPAnalizer.java
+++ src/HTTPAnalizer.java
@@ -13,16 +13,14 @@ class HTTPAnalizer {
     	String result = "";
     	String[] doc = reader.getHTML().split("\n");
     	for (String s: doc) {
-    		if (s.matches(".*utcTime:.*")) {
+    		if (s.matches(".*hours.*")) {
     			result = s;
     		}
     	}
-    	result = result.replaceAll(".*: ", "");
-    	result = result.replaceAll(",.*", "");
-    	GregorianCalendar calendar = new GregorianCalendar();
-    	calendar.setGregorianChange(new Date(new Long(result)));
-    	calendar.setTimeZone(TimeZone.getTimeZone("Russia/Saransk"));
-    	result = calendar.HOUR + ":" + calendar.MINUTE + ":" + calendar.SECOND;
+    	result = result.replaceAll(".*\"hours\">", "");
+    	result = result.replaceAll("class=\"timeview-info\".*", "");
+    	result = result.replaceAll("\\D*","");
+    	result = result.substring(0, 2) + ":" + result.substring(2, 4) + ":" + result.substring(4, 6);
         return result;
     }
 
